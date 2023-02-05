089686924146d4e6c022ad04afc447ee8cb29682
Lee <94841127+galaxian@users.noreply.github.com>
2023-02-06 01:48:45 +0900

    feat(user) : user entity 작성

---
+++ src/user/entity/user.entity.ts
@@ -0,0 +1,23 @@
+export class user {
+  private _email: string;
+  private _userName: string;
+  private _password: string;
+
+  constructor(email: string, userName: string, password: string) {
+    this._email = email;
+    this._userName = userName;
+    this._password = password;
+  }
+
+  get email() {
+    return this._email;
+  }
+
+  get userName() {
+    return this._userName;
+  }
+
+  get password() {
+    return this._password;
+  }
+}
