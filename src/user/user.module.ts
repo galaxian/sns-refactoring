9f668af96a1298f9508959a17b33db3cc286c312
Lee <94841127+galaxian@users.noreply.github.com>
2023-02-07 20:08:23 +0900

    feat(user): provider에 outbound Port 추가

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,6 +3,8 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { SignUpUserController } from './controller/signup-user.controller';
 import { TypeOrmUser } from './entity/typeorm-user.entity';
 import { SIGNUP_USER_INBOUND_PORT } from './inbound-port/signup-user.inbound-port';
+import { SignUpUserRepository } from './outbound-adapter/signup-user.repository';
+import { SIGNUP_USER_OUTBOUND_PORT } from './outbound-port/signup-user.outbound-port';
 import { SignUpUserService } from './service/signup-user.service';
 
 @Module({
@@ -10,6 +12,7 @@ import { SignUpUserService } from './service/signup-user.service';
   controllers: [SignUpUserController],
   providers: [
     { provide: SIGNUP_USER_INBOUND_PORT, useClass: SignUpUserService },
+    { provide: SIGNUP_USER_OUTBOUND_PORT, useClass: SignUpUserRepository },
   ],
 })
 export class UserModule {}
