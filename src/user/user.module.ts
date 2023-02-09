6940fed8820dbad4667d2bd430b88b45e60543c6
Lee <94841127+galaxian@users.noreply.github.com>
2023-02-09 23:26:09 +0900

    feat(user) : provide에 outboundport 추가

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -4,6 +4,8 @@ import { SignUpUserController } from './controller/signup-user.controller';
 import { TypeOrmUser } from './entity/typeorm-user.entity';
 import { SIGNUP_USER_INBOUND_PORT } from './inbound-port/signup-user.inbound-port';
 import { SignUpUserRepository } from './outbound-adapter/signup-user.repository';
+import { GET_USER_BY_EMAIL_OUTBOUND_PORT } from './outbound-port/get-user-by-email.outbound-port';
+import { GET_USER_BY_USERNAME_OUTBOUND_PORT } from './outbound-port/get-user-by-username.outbound-port';
 import { SIGNUP_USER_OUTBOUND_PORT } from './outbound-port/signup-user.outbound-port';
 import { SignUpUserService } from './service/signup-user.service';
 
@@ -13,6 +15,14 @@ import { SignUpUserService } from './service/signup-user.service';
   providers: [
     { provide: SIGNUP_USER_INBOUND_PORT, useClass: SignUpUserService },
     { provide: SIGNUP_USER_OUTBOUND_PORT, useClass: SignUpUserRepository },
+    {
+      provide: GET_USER_BY_EMAIL_OUTBOUND_PORT,
+      useClass: SignUpUserRepository,
+    },
+    {
+      provide: GET_USER_BY_USERNAME_OUTBOUND_PORT,
+      useClass: SignUpUserRepository,
+    },
   ],
 })
 export class UserModule {}
