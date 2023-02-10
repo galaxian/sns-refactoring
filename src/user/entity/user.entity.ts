a72921268d480b80280d660ba02b78f8fe8da28a
Lee <94841127+galaxian@users.noreply.github.com>
2023-02-11 02:25:01 +0900

    feat(user): user entity에 필요 데이터 추가

--- src/user/entity/user.entity.ts
+++ src/user/entity/user.entity.ts
@@ -1,7 +1,11 @@
 export class User {
+  private _id: bigint;
   private _email: string;
   private _userName: string;
   private _password: string;
+  private _createAt: Date;
+  private _updateAt: Date;
+  private _deleteAt: Date;
 
   constructor(email: string, userName: string, password: string) {
     this._email = email;
@@ -9,15 +13,31 @@ export class User {
     this._password = password;
   }
 
-  get email() {
+  get id(): bigint {
+    return this._id;
+  }
+
+  get email(): string {
     return this._email;
   }
 
-  get userName() {
+  get userName(): string {
     return this._userName;
   }
 
-  get password() {
+  get password(): string {
     return this._password;
+  }
+
+  get createAt(): Date {
+    return this._createAt;
+  }
+
+  get updateAt(): Date {
+    return this._updateAt;
+  }
+
+  get deleteAt(): Date {
+    return this._deleteAt;
   }
 }
