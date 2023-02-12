4c95f9f572ad58d5b0040aac8fc2bf54d6ce87a9
Lee <94841127+galaxian@users.noreply.github.com>
2023-02-12 20:28:06 +0900

    feat(user) : provider에 추가

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,9 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { SignUpUserController } from './controller/signup-user.controller';
 import { TypeOrmUser } from './entity/typeorm-user.entity';
 import { SIGNUP_USER_INBOUND_PORT } from './inbound-port/signup-user.inbound-port';
+import { BcryptAdapter } from './outbound-adapter/bcrypt.adapter';
 import { SignUpUserRepository } from './outbound-adapter/signup-user.repository';
 import { GET_USER_BY_EMAIL_OUTBOUND_PORT } from './outbound-port/get-user-by-email.outbound-port';
 import { GET_USER_BY_USERNAME_OUTBOUND_PORT } from './outbound-port/get-user-by-username.outbound-port';
+import { HASH_PASSWORD_OUTBOUND_PORT } from './outbound-port/hash-password.outbound-port';
 import { SIGNUP_USER_OUTBOUND_PORT } from './outbound-port/signup-user.outbound-port';
 import { SignUpUserService } from './service/signup-user.service';
 
@@ -22,6 +24,10 @@ import { SignUpUserService } from './service/signup-user.service';
     {
       provide: GET_USER_BY_USERNAME_OUTBOUND_PORT,
       useClass: SignUpUserRepository,
+    },
+    {
+      provide: HASH_PASSWORD_OUTBOUND_PORT,
+      useClass: BcryptAdapter,
     },
   ],
 })
