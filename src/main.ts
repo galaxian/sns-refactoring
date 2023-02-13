d4595850bf11d74fe033bde46bc377acdb071d8a
Lee <94841127+galaxian@users.noreply.github.com>
2023-02-14 00:30:28 +0900

    feat : validation 전역 설정

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as config from 'config';
+import { ValidationPipe } from '@nestjs/common';
 
 const serverConfig = config.get('server');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   const port = serverConfig.port;
   await app.listen(port);
 }
