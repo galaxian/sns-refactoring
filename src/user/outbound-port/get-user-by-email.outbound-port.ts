74a1017543082df57b2771e53e9f16a5d6eb6c83
Lee <94841127+galaxian@users.noreply.github.com>
2023-02-13 15:47:47 +0900

    refactor(user) : outbound port에서 input/output을 user 객체로 변경

--- src/user/outbound-port/get-user-by-email.outbound-port.ts
+++ src/user/outbound-port/get-user-by-email.outbound-port.ts
@@ -1,14 +1,8 @@
 import { User } from '../entity/user.entity';
 
-export type GetUserByEmailOutboundPortInputDto = string;
-
-export type GetUserByEmailOutboundPortOutputDto = User | null;
-
 export const GET_USER_BY_EMAIL_OUTBOUND_PORT =
   'GET_USER_BY_EMAIL_OUTBOUND_PORT' as const;
 
 export interface GetUserByEmailOutboundPort {
-  getUserByEmail(
-    email: GetUserByEmailOutboundPortInputDto,
-  ): Promise<GetUserByEmailOutboundPortOutputDto>;
+  getUserByEmail(email: string): Promise<User>;
 }
--- src/user/outbound-port/signup-user.outbound-port.ts
+++ src/user/outbound-port/signup-user.outbound-port.ts
@@ -1,20 +1,7 @@
-export type SignUpUserOutboundPortInputDto = {
-  email: string;
-  userName: string;
-  password: string;
-};
-
-export type SignUpUserOutboundPortOutputDto = {
-  id: bigint;
-  email: string;
-  userName: string;
-  password: string;
-};
+import { User } from '../entity/user.entity';
 
 export const SIGNUP_USER_OUTBOUND_PORT = 'SIGNUP_USER_OUTBOUND_PORT' as const;
 
 export interface SignUpUserOutboundPort {
-  excute(
-    params: SignUpUserOutboundPortInputDto,
-  ): Promise<SignUpUserOutboundPortOutputDto>;
+  excute(params: User): Promise<User>;
 }
