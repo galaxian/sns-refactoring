c993aebedb158127647fa3748d68ec329ed26aed
Lee <94841127+galaxian@users.noreply.github.com>
2023-03-09 22:23:15 +0900

    feat(board) : board 모듈 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { typeORMConfig } from './config/typeorm.config';
 import { UserModule } from './user/user.module';
+import { BoardModule } from './board/board.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeORMConfig), UserModule],
+  imports: [TypeOrmModule.forRoot(typeORMConfig), UserModule, BoardModule],
   controllers: [AppController],
   providers: [AppService],
 })
