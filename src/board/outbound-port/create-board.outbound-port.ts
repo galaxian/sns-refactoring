9139e112d2fdf7ce8f89c149954de27bd8527ba7
Lee <94841127+galaxian@users.noreply.github.com>
2023-03-09 22:35:55 +0900

    chore(board) : create board outbound port 오타 수정

--- src/board/outbound-port/create-board.outbound-port.ts
+++ src/board/outbound-port/create-board.outbound-port.ts
@@ -2,6 +2,6 @@ import { Board } from '../entity/board.entity';
 
 export const CREATE_BOARD_OUTBOUND_PORT = 'CREATE_BOARD_OUTBOUND_PORT' as const;
 
-export interface CreatBoardOutboundPort {
+export interface CreateBoardOutboundPort {
   excute(board: Board): Promise<Board>;
 }
